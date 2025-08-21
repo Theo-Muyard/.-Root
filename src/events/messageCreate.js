ae1ed4f5a91706b4ee4cceb15b2db5bf968c4086
Theo-Muyard <97789819+Theo-Muyard@users.noreply.github.com>
2025-08-21 09:19:17 +0200

    Bump

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -12,19 +12,23 @@ export default {
 		const	user = message.member.user;
 		const	DISBOARD_ID = '302050872383242240';
 
+		if (message.author.id === DISBOARD_ID && message.embeds.length > 0) {
+			const embed = message.embeds[0];
+			const description = embed?.description || embed?.data?.description;
 
-		const embed = message.embeds[0];
-		const description = embed?.description || embed?.data?.description;
+			console.log("[DEBUG] embeds:", message.embeds);
+			console.log("[DEBUG] description:", description);
 
-		if (description && description.includes("Bump effectué")) {
-			message.channel.send(`${process.env.LOVE_EMOJI} Thank you for voting!`);
+			if (description && description.includes("Bump effectué")) {
+				message.channel.send(`${process.env.LOVE_EMOJI} Thank you for voting!`);
 
-			setTimeout(() => {
-				const channel = message.guild.channels.cache.get(process.env.CHAT_CHANNEL_ID);
-				if (channel) {
-					channel.send(`${process.env.REMINDER_EMOJI} It's time to vote! Use \`/bump\` with <@${DISBOARD_ID}>.\n*Thanks to everyone who support us!*`);
-				}
-			}, 2 * 60 * 60 * 1000);
+				setTimeout(() => {
+					const channel = message.guild.channels.cache.get(process.env.CHAT_CHANNEL_ID);
+					if (channel) {
+						channel.send(`${process.env.REMINDER_EMOJI} It's time to vote! Use \`/bump\` with <@${DISBOARD_ID}>.\n*Thanks to everyone who support us!*`);
+					}
+				}, 2 * 60 * 60 * 1000);
+			}
 		}
 		if (user.bot) return;
 		xpSystem(user, message);
